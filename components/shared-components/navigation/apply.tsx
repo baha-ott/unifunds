b8359e585c86a8687fe8dcf97a945c0db3855e19
baha-ott <bahaalout126@gmail.com>
2023-10-24 01:45:34 +0300

    Remove logout button from landing

--- components/shared-components/navigation/apply.tsx
+++ components/shared-components/navigation/apply.tsx
@@ -1,19 +1,15 @@
 "use client";
 import Link from "next/link";
-import { Button } from "../../ui/button";
 import BtnPrimary from "../btn-primary";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 const Apply = () => {
-  const supabase = createClientComponentClient();
+
 
   return (
     <div className="flex items-center gap-4">
       <BtnPrimary>
         <Link href="/sign-up">Apply now</Link>
       </BtnPrimary>
-
-      <button onClick={() => supabase.auth.signOut()}>logout</button>
     </div>
   );
 };
